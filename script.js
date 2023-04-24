79dd81a8c4175728260ea42ae305baba63bbc933
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-04-24 17:08:14 +0200

    update: script.js
    adde the function for the remove button

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-const books = [
-    {
-        title: "Fantastic Four",
-        author: "Ben",
-    },
-    {
-        title: "Fantastic Books",
-        author: "Nahid",
-    },
-]
+const books = JSON.parse(localStorage.getItem('form')) || [];
 
-function addBook(book, title, author){
-
-}
-
-function removeBook(book) {
-    let title = "";
-    let author = "";
-
-    for (let i = 0; i < book.length; i++) {
-        if (title === book[i].title && author === book[i].author){
-            delete book[i];
-        }
-    }
-}
-
-function displayBooks(book){
+function displayBooks(book, index){
     let content = `
     <div>
         <p>${book.title}</p>
         <p>${book.author}</p>    
-        <button>Remove</button>
+        <button id ="delete${index}">Remove</button>
         <hr>
     </div>
     `;
     return content;
 }
 
-const container = document.querySelector('.bodyh1');
-
-for (let i = 0; i < books.length; i += 1){
-    console.log(books[i]);
-    let book = displayBooks(books[i]);
-    container.innerHTML += book;
+function storeForm(form) {
+    localStorage.setItem('form', JSON.stringify(form));
 }
 
 let addButton = document.getElementById('addBook');
 let newTitle = document.getElementById('bookTitle');
 let newAuthor = document.getElementById('authorName');
+const container = document.querySelector('.bodyh1');
 
-addButton.addEventListener('click', () => {
-    let title = newTitle.value;
-    let author = newAuthor.value;
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if ( newAuthor.value !== "" && newTitle.value !== ""){
+        let title = newTitle.value;
+        let author = newAuthor.value;
+        let newBook = {
+            title: title,
+            author: author,
+        };
 
-    let newBook = {
-        title: title,
-        author: author,
-    };
-    console.log(newBook);
-    books.push(newBook);
+        books.push(newBook);
+        storeForm(books);
+        location.reload();
+    }
+
+    this.reset();
 })
-console.log(books);
+
+for (let i = 0; i < books.length; i += 1){
+    let book = displayBooks(books[i], i);
+    container.innerHTML += book;
+};
+
+const deleteBtn = document.querySelectorAll('[id^="delete"]');
+deleteBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        let item = button.parentNode;
+        item.parentNode.removeChild(item);
+
+        let id = button.id; 
+
+        for (let i = 0; i < books.length; i++) {
+            if ( id.includes(i) ) {
+                books.splice (i, 1);
+            }
+        }
+
+        console.log(books);
+        storeForm(books);
+    })
+})
