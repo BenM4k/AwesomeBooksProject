40576d9b2b32fc7f5f8eb3abb23aed388f07d12b
Nahid1911 <nahid.raihan>
2023-04-24 21:49:57 +0600

    ESlint error  Correction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const books = JSON.parse(localStorage.getItem('form')) || [];
 
-function displayBooks(book, index){
-    let content = `
+function displayBooks(book, index) {
+  const content = `
     <div>
         <p>${book.title}</p>
         <p>${book.author}</p>    
@@ -9,57 +9,53 @@ function displayBooks(book, index){
         <hr>
     </div>
     `;
-    return content;
+  return content;
 }
 
 function storeForm(form) {
-    localStorage.setItem('form', JSON.stringify(form));
+  localStorage.setItem('form', JSON.stringify(form));
 }
 
-let addButton = document.getElementById('addBook');
-let newTitle = document.getElementById('bookTitle');
-let newAuthor = document.getElementById('authorName');
+const addButton = document.getElementById('addBook');
+const newTitle = document.getElementById('bookTitle');
+const newAuthor = document.getElementById('authorName');
 const container = document.querySelector('.bodyh1');
 
 addButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    if ( newAuthor.value !== "" && newTitle.value !== ""){
-        let title = newTitle.value;
-        let author = newAuthor.value;
-        let newBook = {
-            title: title,
-            author: author,
-        };
+  e.preventDefault();
+  if (newAuthor.value !== '' && newTitle.value !== '') {
+    const title = newTitle.value;
+    const author = newAuthor.value;
+    const newBook = {
+      title,
+      author,
+    };
 
-        books.push(newBook);
-        storeForm(books);
-        
-        location.reload();
-    }
+    books.push(newBook);
+    storeForm(books);
+  }
+  this.reset();
+});
 
-    this.reset();
-})
-
-for (let i = 0; i < books.length; i += 1){
-    let book = displayBooks(books[i], i);
-    container.innerHTML += book;
-};
+for (let i = 0; i < books.length; i += 1) {
+  const book = displayBooks(books[i], i);
+  container.innerHTML += book;
+}
 
 const deleteBtn = document.querySelectorAll('[id^="delete"]');
-deleteBtn.forEach(button => {
-    button.addEventListener('click', () => {
-        let item = button.parentNode;
-        item.parentNode.removeChild(item);
+deleteBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const item = button.parentNode;
+    item.parentNode.removeChild(item);
 
-        let id = button.id; 
+    const { id } = button;
 
-        for (let i = 0; i < books.length; i++) {
-            if ( id.includes(i) ) {
-                books.splice (i, 1);
-            }
-        }
+    for (let i = 0; i < books.length; i += 1) {
+      if (id.includes(i)) {
+        books.splice(i, 1);
+      }
+    }
 
-        console.log(books);
-        storeForm(books);
-    })
-})
+    storeForm(books);
+  });
+});
